214606e0ebb14e55c4dfff7f75bb6f98385029f5
sfilippone93 <santiagofilippone@gmail.com>
2026-06-16 13:23:54 -0300

    aSD

--- generar-pdfs.bat
+++ generar-pdfs.bat
@@ -2,11 +2,11 @@
 set CHROME="C:\Program Files\Google\Chrome\Application\chrome.exe"
 set DIR=%~dp0
 
-echo Generando Santiago_Filippone_CV_EN.pdf...
-%CHROME% --headless --print-to-pdf="%DIR%uploads\Santiago_Filippone_CV_EN.pdf" --no-pdf-header-footer --print-to-pdf-no-header "%DIR%curriculum_en.html"
+echo Generando Santiago_Filippone_Product_Designer_CV_EN.pdf...
+%CHROME% --headless --print-to-pdf="%DIR%uploads\Santiago_Filippone_Product_Designer_CV_EN.pdf" --no-pdf-header-footer --print-to-pdf-no-header "%DIR%curriculum_en.html"
 
-echo Generando Santiago_Filippone_CV_ES.pdf...
-%CHROME% --headless --print-to-pdf="%DIR%uploads\Santiago_Filippone_CV_ES.pdf" --no-pdf-header-footer --print-to-pdf-no-header "%DIR%curriculum_es.html"
+echo Generando Santiago_Filippone_Product_Designer_CV_ES.pdf...
+%CHROME% --headless --print-to-pdf="%DIR%uploads\Santiago_Filippone_Product_Designer_CV_ES.pdf" --no-pdf-header-footer --print-to-pdf-no-header "%DIR%curriculum_es.html"
 
 echo Listo!
 pause
